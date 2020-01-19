1ffa95643aea63c74a7eef59d4d74de7c0aa4c1a
menglin1997 <1351931529@qq.com>
2020-01-19 11:22:38 +0800

    添加横向滑动

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-
+import Json from './Json' //测试用数据
 App.mpType = 'app'
 
 const app = new Vue({
---
+++ pages/1/index.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="content">
+
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="news-nav">
+			<scroll-view scroll-x="true">
+				<text>类目一</text>
+				<text>类目一</text>
+				<text>类目一</text>
+				<text>类目一</text>
+				<text>类目一</text>
+				<text>类目一</text>
+				<text>类目一</text>
+				<text>类目一</text>
+			</scroll-view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '下边是横向滑动',
+				menu: [{
+						id: 0,
+						title: '1'
+					},
+					{
+						id: 1,
+						title: '2'
+					},
+					{
+						id: 2,
+						title: '3'
+					},
+					{
+						id: 4,
+						title: '4'
+					},
+					{
+						id: 5,
+						title: '5'
+					}
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.news-nav {
+		margin: 70rpx 32rpx 0;
+		height: 112rpx;
+		line-height: 112rpx;
+		background-color: #f5f5f5;
+		scroll-view {
+			white-space: nowrap;
+			width: 100%;
+			text {
+				display: inline-block;
+				width: 150rpx;
+				height: 60rpx;
+				text-align: center;
+				font-size: 28rpx;
+				color: #333333;
+				&:nth-child(1) {
+					color: #083C9D;
+					font-size: 32rpx;
+					color: #333333;
+				}
+			}
+		}
+	}
+	
+
+</style>
